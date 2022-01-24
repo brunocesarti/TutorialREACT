9cb9cf4ac992bec79d0378b53117aa98787892bb
Bruno <brunocesarmg@gmail.com>
2022-01-24 09:25:12 -0300

    Atualizando handleClick para jogar fora a historia futura ao voltar no tempo

--- jv/src/index.js
+++ jv/src/index.js
@@ -42,7 +42,7 @@ import './index.css';
   
   class Game extends React.Component {
     handleClick(i){
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]){
@@ -53,6 +53,7 @@ import './index.css';
         history: history.concat([{
           squares: squares,
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     }
